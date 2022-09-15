66a61842189a4b521667a584a1b11e1d3b191d91
Mahtem T <mahtem.school@gmail.com>
2022-09-15 09:29:14 -0700

    printf and its syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("size of a char: %d byte(s)\n", sizeof(c));
 	printf("size of an int: %d byte(s)\n", sizeof(i));
 	printf("size of a long int: %d byte(s)\n", sizeof(li));
