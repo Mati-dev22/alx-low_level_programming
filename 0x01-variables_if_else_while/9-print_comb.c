902f488ec53a9d5482ac02205ac16bc892f56692
Mahtem T <mahtem.school@gmail.com>
2022-09-19 09:53:23 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,19 @@
  * main - prints numbers 1 to 9 with comma and space
  *
  * Description: using the main function
- *this program prints "programming for printing numbers 1-9 with comma and space"
- *Return: 0
+ * this program prints "programming for numbers 1-9 with comma and space"
+ * Return: 0
  */
 int main(void)
 {
-  int i;
+int i;
 
-  for (i = '0'; i <= '9'; i++)
-    putchar(i);
-  if (i != '9')
-    putchar(',');
-  putchar(' ');
+for (i = '0'; i < '10'; i++)
+putchar(i);
+  if (i != '10')
+putchar(',');
+putchar(' ');
 
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
