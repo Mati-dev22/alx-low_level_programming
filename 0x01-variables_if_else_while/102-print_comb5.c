afecbf9701db044720ae15043d340b8aeb2bcad4
Mahtem T <mahtem.school@gmail.com>
2022-09-20 06:48:30 -0700

    numbers as 00 01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,18 @@ int main(void)
 	int i1 = 0;
 	int i2;
 
-	while (i1 != 99) {
+	while (i1 != 99)
+       	{
 		i2 = i1;
 		while (i2 <= 99)
 		{
 			if (i2 != i1)
 			{
-				putchar((i1/10) + 48);
-				putchar((i1 % 10) +48);
+				putchar((i1 / 10) + 48);
+				putchar((i1 % 10) + 48);
 				putchar(' ');
-				putchar((i2/10) + 48);
-				putchar((i2 % 10) +48);
+				putchar((i2 / 10) + 48);
+				putchar((i2 % 10) + 48);
 				if (i1 != 98 || i2 != 98)
 				{
 					putchar(',');
