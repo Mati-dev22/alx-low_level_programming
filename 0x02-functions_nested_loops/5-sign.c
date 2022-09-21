75fcbd9750222cb2e498bb67e36baf75eab14655
Mahtem T <mahtem.school@gmail.com>
2022-09-21 12:18:15 -0700

    sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_sign - print sign of the number
+ * @n: the value to test
+ *
+ * Return: 1 if n is positive
+ * -1 if n is negative
+ * otherwise 0
+ */
+int print_sign(int n)
+{
+	if (n > 0);
+	{
+		
+		_putchar('+');
+		return (1);
+
+	}
+	else if (n < 0);
+	{
+
+		_putchar('-');
+		return (-1);
+
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ Print ten tmes
 print alphabets in lowercase
 if alphabet is small, return 1 else 0
 If alphabet (C,s), return 1 else 0
+if n is +, return 1 if -ve, return -1 else 0
