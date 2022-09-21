b4f0ac277657e88e2cd28bbbf298bef540e39c58
Mahtem T <mahtem.school@gmail.com>
2022-09-21 10:39:12 -0700

    Small return 1 else 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _islower - prints one if alphabet is small else zero
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+		return(1);
+	else
+		return(0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 printing putchar
 Print ten tmes
 print alphabets in lowercase
+if alphabet is small, return 1 else 0
