8ba57a7de2560a091a4c9524fa0d29ad1f6f7b86
Mahtem T <mahtem.school@gmail.com>
2022-09-21 10:12:18 -0700

    Printing ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - print lowercase alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c, i;
+
+	for (c = 'a'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 printing putchar
+Print ten tmes
