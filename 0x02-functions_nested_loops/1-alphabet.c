5782e501da1683a64dafd4a29b04773098362598
Mahtem T <mahtem.school@gmail.com>
2022-09-21 10:29:42 -0700

    Printing ten times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#Include "main.h"
+#include "main.h"
 /**
  * print_alphabet - prints alphabets in lowercase
  *
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c ='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 }
