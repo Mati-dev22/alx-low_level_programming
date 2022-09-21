49a424251694384100ae439908f529f4476d6244
Mahtem T <mahtem.school@gmail.com>
2022-09-21 08:02:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-ifndef MAIN.H
-define MAIN.H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,5 +13,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
