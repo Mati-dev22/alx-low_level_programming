233676cfab184b39a5fd5f2ac8585944c24bb06b
Mahtem T <mahtem.school@gmail.com>
2022-09-21 19:20:51 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, d;
+
+	for (i = 0; i <= 9; i++)
+	{
+		d = (i * j);
+		if ((d / 10) > 0)
+		{
+			_putchar((d / 10) + 48);
+		}
+		else
+		{
+
+			_putchar(' ');
+		}
+
+		_putchar((d % 10) +48);
+		if (j < 9)
+		{
+
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -7,3 +7,4 @@ if n is +, return 1 if -ve, return -1 else 0
 Absolute value of number
 last digit of a number
 Every minute will count
+times table
