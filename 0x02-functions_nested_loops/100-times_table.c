2ff8b4d082ffdddaadb868c6e28d778468ce2057
Mahtem T <mahtem.school@gmail.com>
2022-09-22 00:23:44 -0700

    print umbers with specified dimention

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,34 +9,39 @@ void print_times_table(int n)
 {
 	int i, j ,k;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 1; i <= n; i++)
+	if (n >= 0; && n <= 15)
 	{
 
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
 
-			if (j == 0)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar('0');
-			}
-			else
-			{
-
-				k = (i * j);
-				_putchar((k < 100) ? ' ' : (k / 10 + 48));
-				_putchar((k < 10) ? ' ' : (k / 10 + 48));
-				_putchar((k % 10) + 48);
-				}
-				if (j != n)
-				{
 
 				_putchar(',');
 				_putchar(' ');
+
+				k = i * j;
+
+				if (k <= 99)
+					-putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+
+				if (k >= 100)
+				{
+
+					_putchar((k / 100) +'0');
+					_putchar(((k / 10)) % + '0' );
 				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0')
 			}
-				_putchar('\n');
+			_putchar('\n');
 		}
 	}
+}
