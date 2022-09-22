3f17176d951e75460cc3f878c4d64353611ee30e
Mahtem T <mahtem.school@gmail.com>
2022-09-21 18:26:33 -0700

    Abolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @i: value to test
+ *
+ * Return: absolute value of i
+ */
+int _abs(int)
+{
+	return (i * ((i > 0) - (i < 0)));
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ print alphabets in lowercase
 if alphabet is small, return 1 else 0
 If alphabet (C,s), return 1 else 0
 if n is +, return 1 if -ve, return -1 else 0
+Absolute value of number
